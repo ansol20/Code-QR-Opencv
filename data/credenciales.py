55cc01ad6a57272e6c4b604c0e2176558ab0a05c
Robert Saavedra <rssvedra@gmail.com>
2022-05-12 17:01:16 -0500

    -

--- data/credenciales.py
+++ data/credenciales.py
@@ -6,9 +6,9 @@ import time
 import msvcrt
 
 
-cap = cv2.VideoCapture(0)
-cap.set(3,1920)
-cap.set(4,1080)
+cap = cv2.VideoCapture(0) #Especifica que camara 0 o muchas(1,2,3,4)
+cap.set(3,640) #tamaño del video
+cap.set(4,480)
 
 
 #contador_frames = 0     
@@ -16,7 +16,7 @@ cap.set(4,1080)
                    
 while (cap.isOpened()): 
     success,img=cap.read() 
-    myouput = 'NO Autorizado'
+    myouput = 'NO Autorizado' ### toda imagen pasa como no autorizado, a menos que se valide mydata de la camara a la lista txt 
     color=(0,0,255)
     
     bar_detected = ""
@@ -52,11 +52,13 @@ while (cap.isOpened()):
         cv2.putText(img,myouput,(pts2[0],pts2[1]),cv2.FONT_HERSHEY_COMPLEX,0.9,color,3)
         if myouput == "Autorizado":
             cv2.waitKey(30)
+
+    ###camara escanea ms por frame el cual cada evento debe pulsar una tecla para continuar o habilitando cv2.waitKey(500) a mas ms sea frame por frame
     #contador_frames=codigo_por_frame + 1
     cv2.imshow('Escaneo QR',img) ### abre la imagen
     print("Presione una tecla para continuar...")
     msvcrt.getch()
-    #cv2.waitKey(500) ### tiempo por frame para que escanee el qr
+    cv2.waitKey(500) ### tiempo por frame para que escanee el qr
     #cv2.destroyAllWindows() ### cierra la ventana despues de los 2000 ms de captura
 
     #print("The total number of frames in this video is ", cv2.waitKey(2000))
